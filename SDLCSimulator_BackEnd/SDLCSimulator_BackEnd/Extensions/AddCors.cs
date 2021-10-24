6f931e2c4bca91ee960cb6d28780ad73f8e882aa
Vlad1408EZ <81970879+Vlad1408EZ@users.noreply.github.com>
2021-10-24 23:37:29 +0300

    add cors

---
+++ SDLCSimulator_BackEnd/SDLCSimulator_BackEnd/Extensions/AddCors.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SDLCSimulator_BackEnd.Extensions
+{
+    public static class AddCors
+    {
+        public static void AddAppCors(this IServiceCollection collection)
+        {
+            collection.AddCors(options =>
+            {
+                options.AddPolicy(name: "MyAllowSpecificOrigins",
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin()
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
+                    });
+            });
+        }
+    }
+}
