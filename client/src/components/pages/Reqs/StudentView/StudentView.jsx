10d256d2a2e8483a0ed00e7cf05d2eb1af38e70e
belikx5 <i.bilicenko@gmail.com>
2021-11-29 19:49:43 +0200

    add filter

--- client/src/components/pages/Reqs/StudentView/StudentView.jsx
+++ client/src/components/pages/Reqs/StudentView/StudentView.jsx
@@ -1,18 +1,46 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useLocation, useNavigate } from "react-router";
-import { getStudentTasks } from "../../../../slices/tasksSlice";
+import clx from "classnames";
+
+import cs from "../../../../scss/common.module.scss";
+import CSelect from "../../../common/ui-parts/Select";
+import { FILTER_BY, FILTER_OPTIONS, FILTER_OPTION_TYPE, getStudentTasks, setFilterBy, setFilterOption } from "../../../../slices/tasksSlice";
 import Loading from "../../../common/ui-parts/Loading";
 import Task from "../common/Task";
 import TaskList from "../common/TaskList";
 import TaskExecution from "./TaskExecution/TaskExecutionPage";
+import FlexBox from "../../../common/ui-parts/FlexBox";
+
+const getFilteredTasks = (taskFilterOption, taskFilterBy, tasks) => {
+	if (!taskFilterOption) return tasks;
+	if (taskFilterBy === FILTER_BY.DEFAULT.value) return tasks;
+	if (taskFilterBy === FILTER_BY.DIFFICULTY.value) return tasks.filter(el => el[taskFilterBy] === FILTER_OPTION_TYPE[taskFilterOption]);
+	if (taskFilterBy === FILTER_BY.TYPE.value) return tasks.filter(el => el[taskFilterBy] === FILTER_OPTION_TYPE[taskFilterOption]);
+	// if(taskFilterOption === FILTER_OPTION_TYPE.HARD) return (el) => el.difficulty;
+}
 
 const StudentView = () => {
 	const dispatch = useDispatch();
 	const navigate = useNavigate();
 	const location = useLocation();
-	const { isLoading, studentTasks } = useSelector((state) => state.tasks);
+	const {
+		isLoading,
+		studentTasks,
+		taskFilterBy,
+		taskFilterOption
+	} = useSelector((state) => state.tasks);
 	const [taskId, setTaskId] = useState(null);
+
+	const filteredStudentTasks = useMemo(() => getFilteredTasks(taskFilterOption, taskFilterBy, studentTasks), [taskFilterOption, taskFilterBy, studentTasks]);
+
+	const handleTaskClick = (id) => {
+		navigate(`?taskId=${id}`);
+	};
+
+	const handleFilterByChange = (newValue) => dispatch(setFilterBy(newValue));
+
+	const handleFilterOptionChange = (newValue) => dispatch(setFilterOption(newValue));
 
 	useEffect(() => {
 		if (location.search) {
@@ -25,9 +53,6 @@ const StudentView = () => {
 		dispatch(getStudentTasks());
 	}, []);
 
-	const handleTaskClick = (id) => {
-		navigate(`?taskId=${id}`);
-	};
 
 	if (isLoading) return <Loading />;
 
@@ -38,15 +63,22 @@ const StudentView = () => {
 			execResult={location.state?.result}
 		/>
 	) : (
-		<TaskList listLength={studentTasks.length}>
-			{studentTasks.map((task) => (
-				<Task
-					key={task.id}
-					{...task}
-					onClick={() => handleTaskClick(task.id)}
-				/>
-			))}
-		</TaskList>
+		<div className={clx(cs.width600, cs.marginAutoHorizontal)}>
+			<FlexBox className={cs.marginTop50}>
+				<CSelect label="Фільтрувати" value={taskFilterBy} onChange={handleFilterByChange} options={Object.values(FILTER_BY)} />
+				<CSelect label="Опції фільтру" value={taskFilterOption} onChange={handleFilterOptionChange} options={FILTER_OPTIONS[taskFilterBy.toUpperCase()]} />
+			</FlexBox>
+			<TaskList listLength={filteredStudentTasks.length}>
+				{filteredStudentTasks.map((task) => (
+					<Task
+						key={task.id}
+						{...task}
+						onClick={() => handleTaskClick(task.id)}
+					/>
+				))}
+			</TaskList>
+		</div>
+
 	);
 };
 
